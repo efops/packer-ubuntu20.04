21ae35aef8bcdbe18952cf48618546856a642e96
Efkan Isazade <efkan.isazade@gmail.com>
2021-04-14 16:31:00 +0400

    Update setup_ubuntu2004.sh

--- scripts/setup_ubuntu2004.sh
+++ scripts/setup_ubuntu2004.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-apt-get install -f -y python net-tools curl cloud-init python3-pip
-
-# Install VMWare Guestinfo Cloud-init source, used by rancher
-curl -sSL https://raw.githubusercontent.com/vmware/cloud-init-vmware-guestinfo/master/install.sh | sh -
-
-# Reset Machine-ID
-# http://manpages.ubuntu.com/manpages/bionic/man5/machine-id.5.html
-# This is the equivalent of the SID in Windows
-# Netplan also uses this as DHCP Identifier, causing multiple VMs from the same Image
-# to get the same IP Address
-rm -f /etc/machine-id
-
-# Reset Cloud-init state
-# systemctl stop cloud-init
-# rm -rf /var/lib/cloud/
-cloud-init clean -s -l
+echo '> Cleaning all audit logs ...'
+if [ -f /var/log/audit/audit.log ]; then
+cat /dev/null > /var/log/audit/audit.log
+fi
+if [ -f /var/log/wtmp ]; then
+cat /dev/null > /var/log/wtmp
+fi
+if [ -f /var/log/lastlog ]; then
+cat /dev/null > /var/log/lastlog
+fi
+# Cleans SSH keys.
+echo '> Cleaning SSH keys ...'
+rm -f /etc/ssh/ssh_host_*
+# Sets hostname to localhost.
+echo '> Setting hostname to localhost ...'
+cat /dev/null > /etc/hostname
+hostnamectl set-hostname localhost
+# Cleans apt-get.
+echo '> Cleaning apt-get ...'
+apt-get clean
+# Cleans the machine-id.
+echo '> Cleaning the machine-id ...'
+truncate -s 0 /etc/machine-id
+rm /var/lib/dbus/machine-id
+ln -s /etc/machine-id /var/lib/dbus/machine-id
